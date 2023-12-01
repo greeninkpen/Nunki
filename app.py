2ae8e971e63b0f0db37e579e23da85c1065c7e53
Morag Bisset <m.bisset@hotmail.co.uk>
2023-12-01 13:05:23 +0000

    API Call to crease a simple sentence using the AI

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, jsonify, request
+from openai import OpenAI
+from config import api_key
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,3 +10,22 @@ def hello():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+
+
+class APICall:
+    def api_call(self):
+        client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key=api_key,
+        )
+
+        chat_completion = client.chat.completions.create(
+         messages=[{"role": "user", "content":
+        "Write me a short sentense that contains at least three of the eight parts of speech for an A1 English learner"}],
+
+        model="gpt-3.5-turbo",
+        )
+        return chat_completion
+
+
