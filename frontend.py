e859bc0726f87ae3709ee24a80cc9c45321f6fe9
Morag Bisset <m.bisset@hotmail.co.uk>
2023-12-01 11:12:05 +0000

    Creating draft FE file

---
+++ frontend.py
@@ -0,0 +1,22 @@
+#absolutely not the duolingo owl
+welcome_owl = '''
+   \\_\\__  
+   (o,o) 
+   { ` } 
+ -"--"--
+'''
+
+#Draft message- please don't actually use this- this was just to test how it looked
+welcome_message= ''' 
+Welcome to the Language Learning Game\n This game is designed to help native English Speakers learn the technicalities of the English langauge. 
+\n Can you tell a 'noun' from an 'adjective'? Or what the 'definitive article' is? blah blah \n 
+Then this game is for you! \n \n
+ 
+ +++GAME RULES+++\n
+ 
+ ADD MORE INFO HERE "'''
+
+#Alejandra & Maria- creative eye over this please!
+
+print(welcome_owl)
+print (welcome_message)
