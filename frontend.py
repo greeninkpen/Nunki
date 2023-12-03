464d49ecc02ee0ef34991f238ab18dfcd054d50c
Morag Bisset <m.bisset@hotmail.co.uk>
2023-12-03 09:24:41 +0000

    Updated FE with Maria's awesome suggestions

--- frontend.py
+++ frontend.py
@@ -1,28 +1,51 @@
-#absolutely not the duolingo owl
-welcome_owl = '''
-   \\_\\__  
-   (o,o) 
-   { ` } 
- -"--"--
+
+welcome_fox = '''
+   /\   /\   
+  //\\_//\\     ____
+  \_     _/    /   /
+   / * * \    /^^^]
+   \_\O/_/    [   ]
+    /   \_    [   /
+    \     \_  /  /
+     [ [ /  \/ _/
+    _[ [ \  /_/
 '''
 
-#Draft message- please don't actually use this- this was just to test how it looked
 welcome_message= ''' 
-Welcome to the Language Learning Game\n Learning English grammar rules can be challenging for non-native and native speakers alike.\n
-This game is intended as an educational resource for both learners and teachers, and those wanting to improve their own skills and knowledge.
+Welcome to the Language Learning Odyssey! 🌟\n
 
-\n Can you tell a 'noun' from an 'adjective'? Or what an 'article' is? blah blah \n 
-Test your knowledge with this mini-quiz \n \n
- 
- +++GAME RULES+++\n
- 
-You will be shown a simple phrase, and you've to guess which word is the noun, or adjective etc\n
-  If you're not sure how to tell the difference between a noun or an article, a handy glossary is printed below: "'''
+Embark on an exhilarating quest to conquer English grammar in our Language Learning Game!\n\n
 
-#Alejandra & Maria- creative eye over this please!
++++ 🎮 GAME ON 🎮 +++
+\n\n
+What Awaits You:\n
 
-print(welcome_owl)
+    🌈 Dive into enchanting phrases and unveil the hidden parts of speech. Are you up for the challenge?\n
+    🔍 Think you can unravel the mystery of distinguishing a 'noun' from an 'adjective'?\n
+    🌟 Face captivating challenges to test your prowess with nouns, adjectives, and beyond!\n
+    🧠 Experience interactive quizzes crafted for learners, teachers, and language aficionados!\n
+\n \n'''
+
+game_rules ='''
++++ 🚀 UNLEASH THE THRILLING RULES! 🚀 +++\n\n
+
+Ready to embark on a linguistic journey? The adventure begins now!\n\n
+
+🌟 Game Rules 🌟\n
+
+    Encounter mind-bending phrases – your mission is to unveil the secret roles of words as nouns, adjectives, and more!\n
+    Dare to explore the mesmerizing realm of English grammar with challenges that spark excitement!\n
+    Lost between a noun and an article? Fear not! A whimsical glossary awaits below, ready to transform you into a language maestro.\n\n
+    '''
+glossary_message= ''' +++ 🌈 WHIMSICAL GLOSSARY 🌈 +++\n\n
+
+Seize the chance to elevate your language skills in a unique way\n
+
+🚀 Hit play now and set forth on your Language Learning Odyssey!\n'''
+
+print(welcome_fox)
 print (welcome_message)
-
+print (game_rules)
+print (glossary_message)
 #print (db-glossary)
 
