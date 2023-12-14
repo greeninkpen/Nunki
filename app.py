38f1860b37a2f23f9df2d17c21927824721c1301
Desiree Lagerquist <lagerquistd@yahoo.com>
2023-12-14 17:29:15 +0100

    deleted commented code from app and db_utils

--- app.py
+++ app.py
@@ -81,47 +81,6 @@ def save_phrase():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 
-
-# @app.route('/sentence', methods=['POST'])
-# def save_sentence():
-#     sentence_text = sentence_data["sentence"]
-#     #   SAVE IT TO DB using connector
-#
-#     if sentence_text:
-#         db_add_sentence(sentence_text)
-#         return jsonify({'message': 'Sentence saved successfully'}), 200
-#     else:
-#         return jsonify({'error': "Missing 'sentence' in request data"}), 400
-#
-#
-# @app.route('/words', methods=['POST'])
-# def save_words():
-#     try:
-#         # sentence_id = data.get('sentence_id') #sentence_id not present in data
-#         words = sentence_data["words"]
-#
-#         # if not (sentence_id and words):
-#         if not words:
-#             return jsonify({'error': 'Missing required data'}), 400
-#
-#         for word_data in words:
-#             word_text = word_data['word']
-#             part_of_speech = word_data['part_of_speech']
-#
-#             if word_text and part_of_speech:
-#                 # db_add_words(sentence_id, word_text, part_of_speech)
-#                 db_add_words(word_text, part_of_speech)
-#
-#             else:
-#                 return jsonify({'error': 'Missing required word data'}), 400
-#
-#         return jsonify({'message':'Words and parts of speech saved successfully'}), 200
-#
-#     except Exception as e:
-#         print(f'Error: {e}')
-#         return jsonify({'error': 'Failed to save words and parts of speech'}), 500
-
-
 # app.run must always go at the end of the file in order for the endpoints to 'exist'
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
