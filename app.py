39c8582326625fa5f85e1893efb3cccc4617ce15
greeninkpen <80480469+greeninkpen@users.noreply.github.com>
2023-12-17 23:26:43 +0100

    Updated get_random_sentence and get_game_dictionary

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import json
 from flask import Flask, jsonify, request
 from openai import OpenAI
 from config import api_key
-from db_utils import db_add_sentence_and_words, get_sentence, get_glossary, get_game_dict
+from db_utils import db_add_sentence_and_words, db_get_sentence, db_get_game_dict
+
 
 app = Flask(__name__)
 
@@ -41,9 +42,9 @@ class APICall:
         """
 
         chat_completion = client.chat.completions.create(
-         messages=[{"role": "user", "content": prompt}],
-         response_format={"type": "json_object"},
-         model="gpt-3.5-turbo-1106",
+            messages=[{"role": "user", "content": prompt}],
+            response_format={"type": "json_object"},
+            model="gpt-3.5-turbo-1106",
         )
         return chat_completion
 
@@ -81,22 +82,45 @@ def save_phrase():
         print(f"Error processing message: {e}")
         return jsonify({"status": "error", "message": str(e)}), 500
 
-#DRAFT GET REQUEST
-@app.route("/get_sentence", methods=["GET"])
+
 def get_random_sentence():
     # Try to get a random sentence from the database
-    sentence = get_sentence()
+    sentence = db_get_sentence()
+    print("Random sentence: ", sentence)
 
     # Testing until sentences are available in the DB
     if not sentence:
         sample_sentence = "This is a sample sentence."
-        return jsonify({"sentence": sample_sentence})
+        return sample_sentence
+    return sentence
 
-    return jsonify({"sentence": sentence})
 
+@app.route("/get_sentence", methods=["GET"])
+def get_random_sentence_rest():
+    # Try to get a random sentence from the database
+    sentence = get_random_sentence()
+    return jsonify(sentence)
+
+
+def get_game_dictionary():
+    try:
+        game_phrase_dict = db_get_game_dict(db_get_sentence)
+        return game_phrase_dict
+    except Exception as e:
+        print(f"Error in calling the glossary: {str(e)}")
+        return "error"
+
+
+@app.route("/get_game_dictionary", methods=["GET"])
+def get_game_dictionary_rest():
+    try:
+        game_phrase_dict = get_game_dictionary()
+        return jsonify(game_phrase_dict)
+    except Exception as e:
+        print(f"Error in calling the glossary: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
 
 
 # app.run must always go at the end of the file in order for the endpoints to 'exist'
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-
