a4e73810efd57070f45581db60a45ae5acac5392
greeninkpen <80480469+greeninkpen@users.noreply.github.com>
2023-12-17 23:38:32 +0100

    clarified comments config.py

--- config.py
+++ config.py
@@ -7,8 +7,10 @@ HOST = "127.0.0.1"
 USER = "root"
 
 PASSWORD = os.getenv("PASSWORD")
-#REMEBER TO ADD YOUR OWN PASSWORD HERE!!! AND INCLUDE IN GIT IGNORE SO THAT NO SENSITIVE INFO IS SHARED!!
+#Remember to add your mysql workbench PASSWORD to the .env PASSWORD variable
 
 api_key = os.getenv("OPENAI_API_KEY")
-#Add your OpenAI password here
+#Add your personal OpenAI API KEY to the .env OPENAI_API_KEY variable
 
+#REMEMBER TO CREATE THE .ENV FILE WITH THESE TWO VARIABLE
+#AND INCLUDE THE .ENV IN .GITIGNORE SO THAT NO SENSITIVE INFO IS SHARED!!
